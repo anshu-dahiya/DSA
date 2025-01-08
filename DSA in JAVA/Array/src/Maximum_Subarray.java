3e2fcd751abc76188deea3214bebe769da306128
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-01-08 21:29:00 +0530

    Maximum_Subarray(Kadane's Algorithm)

---
+++ DSA in JAVA/Array/src/Maximum_Subarray.java
@@ -0,0 +1,48 @@
+//Given an integer array nums, find the subarray with the largest sum,
+//and return its sum.
+
+//Example 1: Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+//Output: 6
+//Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+//Example 2:
+//Input: nums = [1]
+//Output: 1
+//Explanation: The subarray [1] has the largest sum 1.
+
+//Example 3:
+//Input: nums = [5,4,-1,7,8]
+//Output: 23
+//Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+public class Maximum_Subarray {
+    public int maxSubarray(int[] nums){
+
+        int sum = 0;
+        int maxSum = nums[0];
+
+        for(int i=0; i<nums.length; i++){
+
+            sum = sum + nums[i];
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+    public static void main(String[] args){
+        Maximum_Subarray ms = new Maximum_Subarray();
+
+        int[] nums1 = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println("Max Subarray Sum: "+ms.maxSubarray(nums1));
+
+        int[] nums2 = {1};
+        System.out.println("Max Subarray Sum: "+ms.maxSubarray(nums2));
+
+        int[] nums3 = {5,4,-1,7,8};
+        System.out.println("Max Subarray Sum: "+ms.maxSubarray(nums3));
+    }
+}
--- DSA in JAVA/Array/src/Two_Sum.java
+++ DSA in JAVA/Array/src/Two_Sum.java
@@ -1,5 +1,18 @@
 //Given an array of integers "nums" and an integer "target",
 //return index of the two numbers whose sum is equals to target.
+
+//Example 1:
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+//Example 2:
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+
+//Example 3:
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
 import java.util.*;
 
 public class Two_Sum {
@@ -17,10 +30,20 @@ public class Two_Sum {
     }
 
     public static void main(String[] args) {
-        int[] nums = {9, 12, 6, 8, 2, 10};
-        int target = 14;
-        System.out.println(Arrays.toString(nums));
-        System.out.println(Arrays.toString(twoSum(nums, target)));
+        int[] nums1 = {2,7,11,15};
+        int target1 = 9;
+        System.out.println("Array1: " + Arrays.toString(nums1));
+        System.out.println("Array1 target index: " + Arrays.toString(twoSum(nums1, target1)));
         //Arrays.toString method is used to convert an array into a human-readable string representation.
+
+        int[] nums2 = {3,2,4};
+        int target2 = 6;
+        System.out.println("Array2: " + Arrays.toString(nums2));
+        System.out.println("Array2 target index: " + Arrays.toString(twoSum(nums2, target2)));
+
+        int[] nums3 = {3,3};
+        int target3 = 6;
+        System.out.println("Array3: " + Arrays.toString(nums3));
+        System.out.println("Array3 target index: " + Arrays.toString(twoSum(nums3, target3)));
     }
 }
