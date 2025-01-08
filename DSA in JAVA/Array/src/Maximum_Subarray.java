cf17f4454eaf56efe700d9b27d843f1051fb748a
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-01-08 21:30:19 +0530

    Maximum_Subarray(Kadane's Algorithm)

--- DSA in JAVA/Array/src/Maximum_Subarray.java
+++ DSA in JAVA/Array/src/Maximum_Subarray.java
@@ -37,12 +37,12 @@ public class Maximum_Subarray {
         Maximum_Subarray ms = new Maximum_Subarray();
 
         int[] nums1 = {-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println("Max Subarray Sum: "+ms.maxSubarray(nums1));
+        System.out.println("Max Subarray Sum: " + ms.maxSubarray(nums1));
 
         int[] nums2 = {1};
-        System.out.println("Max Subarray Sum: "+ms.maxSubarray(nums2));
+        System.out.println("Max Subarray Sum: " + ms.maxSubarray(nums2));
 
         int[] nums3 = {5,4,-1,7,8};
-        System.out.println("Max Subarray Sum: "+ms.maxSubarray(nums3));
+        System.out.println("Max Subarray Sum: " + ms.maxSubarray(nums3));
     }
 }
